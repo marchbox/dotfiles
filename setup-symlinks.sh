9694fb909cb7f7f7923423fa39728a3f3cd139dd
Zacky Ma <zacky@marchbox.com>
2024-04-16 22:35:32 -0700

    add navi config and cheats

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -5,5 +5,6 @@ ln -s ~/dotfiles/alacritty ~/.config/
 ln -s ~/dotfiles/fish ~/.config/
 ln -s ~/dotfiles/helix ~/.config/
 ln -s ~/dotfiles/zellij ~/.config/
+ln -s ~/dotfiles/navi ~/.config/
 
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
