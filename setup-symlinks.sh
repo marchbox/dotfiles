3434f5a84902a20eb43d757104935bb9e2087ba5
Zacky Ma <zacky@marchbox.com>
2024-04-16 22:46:07 -0700

    add gitu config

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -6,5 +6,6 @@ ln -s ~/dotfiles/fish ~/.config/
 ln -s ~/dotfiles/helix ~/.config/
 ln -s ~/dotfiles/zellij ~/.config/
 ln -s ~/dotfiles/navi ~/.config/
+ln -s ~/dotfiles/gitu ~/.config/
 
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
