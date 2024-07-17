a8d258bf31a414334d020a2f26abd1ade553f248
Zacky Ma <machi@microsoft.com>
2024-07-17 15:46:20 -0700

    remove gitu

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -6,7 +6,6 @@ ln -s ~/dotfiles/fish ~/.config/
 ln -s ~/dotfiles/helix ~/.config/
 ln -s ~/dotfiles/zellij ~/.config/
 ln -s ~/dotfiles/navi ~/.config/
-ln -s ~/dotfiles/gitu ~/.config/
 ln -s ~/dotfiles/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
 
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
