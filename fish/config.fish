978f0d0baa6fd201a9c390f662312e2b30df66f9
Zacky Ma <machi@microsoft.com>
2025-03-17 15:08:33 -0700

    add y for yazi

--- fish/config.fish
+++ fish/config.fish
@@ -51,5 +51,15 @@ bind -M insert \cd accept-autosuggestion
 # fzf key bindings
 fzf --fish | source
 
+# yazi
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
 # Starship prompt
 starship init fish | source
