1675bbdc2575ed46f4e24f574d1d9459e2fdb22a
Zacky Ma <machi@microsoft.com>
2025-09-12 15:31:17 -0700

    update fish aliases

--- fish/config.fish
+++ fish/config.fish
@@ -22,7 +22,7 @@ abbr --add v hx
 abbr --add h hx
 abbr --add zz zellij
 abbr --add za zellij a
-abbr --add ggg lazygit
+abbr --add gg lazygit
 abbr --add n npm
 abbr --add nn npm run
 abbr --add nnd npm run dev
@@ -34,6 +34,7 @@ abbr --add ff fzf
 abbr --add g git
 abbr --add gst git status
 abbr --add gc git commit
+abbr --add gcm git commit -m
 abbr --add gcmsg git commit -m
 abbr --add ga git add
 abbr --add gd git diff
@@ -41,6 +42,7 @@ abbr --add gds git diff --staged
 abbr --add gb git branch
 abbr --add gco git checkout
 abbr --add gp git push
+abbr --add gpf git push --force-with-lease
 abbr --add grhh git reset --hard
 abbr --add gl git pull
 abbr --add gcl git clean -df
